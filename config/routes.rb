5e5e403467ff45ae21d4900a43634e641dccfbc1
amaury dav <amauryat@e-nautia.com>
2023-06-15 17:11:30 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :returns, only: [:index, :create, :new, :update]
+  get '/logistics', to: 'pages#logistic'
   # Defines the root path route ("/")
   # root "articles#index"
 end
