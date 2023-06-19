32315a3d63635851ee727a5355ca85bacdcb5dda
amaury dav <amauryat@e-nautia.com>
2023-06-19 21:20:42 +0200

    Create orders, order_items, and return_items tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_100739) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_190738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "consumers", force: :cascade do |t|
-    t.string "command_number"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.integer "phone_number"
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "client_name"
+    t.integer "order_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "return_items", force: :cascade do |t|
+    t.bigint "return_id"
+    t.bigint "order_item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_item_id"], name: "index_return_items_on_order_item_id"
+    t.index ["return_id"], name: "index_return_items_on_return_id"
   end
 
   create_table "returns", force: :cascade do |t|
@@ -54,4 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_100739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "return_items", "order_items"
+  add_foreign_key "return_items", "returns"
 end
