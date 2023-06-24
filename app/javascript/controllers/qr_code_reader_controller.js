f65b0ca9832be8ce14a7bedf4f45a19fc0a9e589
amaury dav <amauryat@e-nautia.com>
2023-06-24 16:37:09 +0200

    creer puis lire qr-code

---
+++ app/javascript/controllers/qr_code_reader_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "stimulus";
+import { BrowserQRCodeReader } from "@zxing/library";
+import Rails from "@rails/ujs";
+
+export default class extends Controller {
+  static targets = ["video"];
+
+  connect() {
+    this.codeReader = new BrowserQRCodeReader();
+    this.startDecoding();
+  }
+
+  startDecoding() {
+    this.codeReader
+      .decodeFromInputVideoDevice(undefined, this.videoTarget)
+      .then((result) => {
+        let qrDataFromReader = result.text;
+        let formData = new FormData();
+        let qrCodeParams = {
+          qr_data: qrDataFromReader
+        };
+        formData.append("qr_code_json_data", JSON.stringify(qrCodeParams));
+
+        Rails.ajax({
+          url: "/qr_codes",
+          type: "post",
+          data: formData
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  disconnect() {
+    this.codeReader.reset();
+  }
+}
--- app/models/return.rb
+++ app/models/return.rb
@@ -4,4 +4,23 @@ class Return < ApplicationRecord
 
   has_many :return_items
   accepts_nested_attributes_for :return_items
+
+  after_create_commit :create_qr_code
+
+  private
+
+
+  def create_qr_code
+    qrcode = RQRCode::QRCode.new("Return-#{id}")
+    png = qrcode.as_png(size: 300)
+    save_qr_code_locally(png)
+  end
+
+  def save_qr_code_locally(png)
+    folder_path = Rails.root.join('public', 'qr_codes')
+    FileUtils.mkdir_p(folder_path) unless File.directory?(folder_path)
+
+    file_path = folder_path.join("return-#{id}.png")
+    File.open(file_path, 'wb') { |f| f.write(png.to_s) }
+  end
 end
