d9d24c58f2b6d36dc09c386a99b39f56d8da30e2
celine savary <cmsavary@yahoo.fr>
2023-06-27 20:52:57 +0200

    front in progress

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link videoblocks-steadycam-shot-of-big-warehouse-with-many-goods-for-building-or-repair-shopping-mall-or-stock-with-stuff-for-decoration_r8zf9u7zwz__f0dcea737d629327c0ac627221b9660e__P360.mp4
